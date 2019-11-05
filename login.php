1873ece6219e97eb073fb6ef32bf7023f4f8c801
Abinavraj5427 <abinavraj10@gmail.com>
2019-11-05 16:28:14 -0600

    Working PHP with Axios

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
-    $username = $_POST['username'];
-    // $password = $_POST['password'];
-    // var_dump($username);
-    echo "HELLO";
+    $rest_json = file_get_contents("php://input");
+    $_POST = json_decode($rest_json, true);
+    echo $_POST['username'];
 ?>
