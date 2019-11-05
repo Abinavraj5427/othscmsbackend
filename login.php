86848cf276ce80be543df62b7652e6c945352a7e
Abinavraj5427 <abinavraj10@gmail.com>
2019-11-05 07:48:24 -0600

    Init commit

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    $username = $_POST['username'];
+    // $password = $_POST['password'];
+    // var_dump($username);
+    echo "HELLO";
+?>
