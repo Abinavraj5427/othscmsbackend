d420716ec705643c1930b94fcca4e0ad3211e7b9
Abinavraj5427 <abinavraj10@gmail.com>
2019-11-05 17:13:58 -0600

    checks if you are admin

--- login.php
+++ login.php
@@ -3,5 +3,10 @@
     header("Access-Control-Allow-Headers: *");
     $rest_json = file_get_contents("php://input");
     $_POST = json_decode($rest_json, true);
-    echo $_POST['username'];
+    $username =  $_POST['username'];
+    $password = $_POST['password'];
+    if($username === 'admin' && $password == 'admin')
+        echo 'success';
+    else
+        echo 'failure';
 ?>
