eeece61be5fde2d7ea4c5f63926d13d1ed99ce06
Abinavraj5427 <abinavraj10@gmail.com>
2019-11-11 18:10:22 -0600

    Cookie login with confirmation works

--- confirmlogin.php
+++ confirmlogin.php
@@ -6,10 +6,27 @@
     //HTTP inputs
     $rest_json = file_get_contents("php://input");
     $_POST = json_decode($rest_json, true);
-    $username =  $_POST['username'];
-    $password = $_POST['password'];
+    $token = $_POST['authtoken'];
+
 
     //connecting to SQL Database
     $con = mysqli_connect("localhost", "root", "", "othscmsdb");
+
+    //looking for matching username
+    $sql = "SELECT * FROM users WHERE auth_token=\"$token\"";
+    $res = mysqli_query($con, $sql);
+
+
+    //checking if password is correct
+    if(mysqli_num_rows($res)>0){
+        while($row = mysqli_fetch_assoc($res)){
+            $response = array('authenticated' => true, 'role' => $row['role']);
+            echo json_encode($response);
+            exit;
+        }
+    }
+
+    $response = array('authenticated' => false);
+    echo json_encode($response);
 
 ?>
