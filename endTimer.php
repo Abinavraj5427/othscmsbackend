a5d889a6f62995fafe4ab461dba0605da2ecaa6f
JavaLich <javalich@outlook.com>
2019-12-06 07:40:42 -0600

    End timer added

---
+++ endTimer.php
@@ -0,0 +1,18 @@
+<?php
+    //CORS Headers
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+
+    //HTTP inputs
+    $rest_json = file_get_contents("php://input");
+    $_POST = json_decode($rest_json, true);
+    $con = mysqli_connect("localhost", "root", "", "othscmsdb");
+    $time = 0;
+    $sql =
+    "UPDATE timer
+    SET timer = $time
+    ";
+
+    $res = mysqli_query($con, $sql);
+    echo "SUCCESS";
+?>
