c7b2407cc3108c831a1472ffc26f0aeb0bfc1a92
JavaLich <javalich@outlook.com>
2020-01-24 08:04:35 -0600

    Commented stuff

---
+++ runSubmission.php
@@ -0,0 +1,20 @@
+<?php
+
+//CORS Headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+/*
+//HTTP inputs
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
+
+$file = $_POST['filePath'];
+$name = $_POST['problem'];
+$inputPath = "input/".strtolower($name).".in";
+copy($inputPath, strtolower(substr($file, 0, strlen($file)-5)).".in");
+
+echo exec("set path=C:\wamp64\www\othscmsbackend\jdk1.8.0_201\bin");
+echo exec('javac '.$file." 2>&1");
+echo exec('java '.substr($file, 0, strlen($file)-5)." 2>&1");
+*/
+?>
